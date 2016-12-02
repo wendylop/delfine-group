29e259a1249659d3ff49316268a68b4616ec6799
Lilian Hung <l.hung.m@gmail.com>
2016-12-02 11:07:58 +0100

    Aufg.4 – added Alice's methods

--- broker-model/src/de/sb/broker/rest/AuctionService.java
+++ broker-model/src/de/sb/broker/rest/AuctionService.java
@@ -8,14 +8,27 @@ import javax.persistence.Cache;
 //import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.RollbackException;
 import javax.persistence.TypedQuery;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response.Status;
+
 import de.sb.broker.model.Auction;
+import de.sb.broker.model.Bid;
 import de.sb.broker.model.Person;
 
 @Path("/auctions")
@@ -84,55 +97,64 @@ public class AuctionService {
 	* Note that an auction may only be modified as long as it is not sealed 
 	* (i.e. is open and still without bids).
 	*
-	* PUT /auctions: 
+	* PUT /auctions (Aufg 4): 
 	* Additionally constrain that an auction may only be altered 
 	* if the requester is the same as the auction's seller.
 	*/
 	@PUT
-	public void putAuctions(Auction template){
-		EntityManager em = LifeCycleProvider.brokerManager();
+	@Consumes({"application/xml", "application/json"})
+	public Long putAuctions(
+			Auction template,
+			@HeaderParam("Authorization") final String authentication
+			){
 		
+		EntityManager em = LifeCycleProvider.brokerManager();
+		final Person requester = LifeCycleProvider.authenticate(authentication);
+		final boolean persist = template.getIdentity() == 0;
+		final Auction auction;
+				
 		try {
-			final boolean insert = template.getIdentity() == 0;
-			/*
-			 * TODO implement
-			 */
-	
-			TypedQuery<Long> qa = em.createQuery("select a.identity from Auction as a where"
-					+ "(:title is null or a.title = :title) and"
-					+ "(:unitCount is null or a.unitCount = :unitCount) and"
-					+ "(:askingPrice is null or a.askingPrice = :askingPrice) and"
-					+ "(:closureTimestamp is null or a.closureTimestamp = :closureTimestamp) and"
-					+ "(:description is null or a.description = :description) and"
-					+ "(:seller is null or a.seller = :seller)" , 
-					Long.class);
-			
-			qa.setParameter("title", template.getTitle());
-			qa.setParameter("unitCount", template.getUnitCount());
-			qa.setParameter("askingPrice", template.getAskingPrice());
-			qa.setParameter("closureTimestamp", template.getClosureTimestamp());
-			qa.setParameter("description", template.getDescription());
-			qa.setParameter("seller", template.getSeller());
-	
-			
-			
-			List<Long> auctionIds = qa.getResultList();
-			List<Auction> auctions = new ArrayList<Auction>();
-			
-			//for-Schleife über alle IDs
-			for(Long id : auctionIds){
-				auctions.add(em.find(Auction.class, id));
-			}
-			
+			//final boolean insert = template.getIdentity() == 0;
+						
 			//Frage: Wie Zugriff auf alle Auctions
 			
 			//Verwendung? cache = em.getEntityManagerFactory().getCache();
 			//cache.evict(entity.getClass(), entity.getIdentity());
 			
+			if(persist){
+				auction = new Auction(requester);
+			} else {
+				auction = em.find(Auction.class, template.getIdentity());
+				if (auction == null) throw new NotFoundException();
+				if (requester.getIdentity() != auction.getSellerReference()) throw new ForbiddenException();
+				if (auction.isSealed()) throw new ForbiddenException();
+			}
+			
+			auction.setTitle(template.getTitle());
+			auction.setDescription(template.getDescription());
+			auction.setClosureTimestamp(template.getClosureTimestamp());
+			auction.setAskingPrice(template.getAskingPrice());
+			auction.setUnitCount(template.getUnitCount());
+			auction.setVersion(template.getVersion());
+			
+		
+			if (persist){
+				em.persist(auction);	
+			} else {
+				em.flush();
+			}
+
 			em.getTransaction().commit();
+			
+		} catch (ConstraintViolationException e) {
+			throw new ClientErrorException(Status.BAD_REQUEST);
+		} catch (RollbackException e) {
+			throw new ClientErrorException(Status.CONFLICT);
 		} finally {
 			em.getTransaction().begin();
 		}
+	
+		return auction.getIdentity();
 	}
 
 	/*
@@ -153,6 +175,24 @@ public class AuctionService {
 	 * GET /auctions/{identity}/bid (new): 
 	 * Returns the requester's bid for the given auction, or null if none exists.
 	 */
+	@GET
+	@Path("/{identity}/bid")
+	@Produces({"application/xml", "application/json"})
+	//@Bid.XmlBidderAsReferenceFilter
+	//@Bid.XmlAuctionAsReferenceFilter
+	public Bid getBidForAuction(
+			@PathParam("identity") long identity,
+			@HeaderParam("Authorization") final String authentication
+			) {
+		
+		final Person requester = LifeCycleProvider.authenticate(authentication);
+		for (Bid bid : requester.getBids()) {
+			if (bid.getAuction().getIdentity() == identity){
+				return bid;
+			}
+		}
+		return null;
+	}
 	
 	/*
 	 * POST /auctions/{identity}/bid (new): 
@@ -160,7 +200,56 @@ public class AuctionService {
 	 * depending on the requester and the price (in cent) within the given request body. 
 	 * If the price is zero, then the requester's bid is removed instead.
 	 */
-	
+	@POST
+	@Path("/{identity}/bid")
+	@Consumes({"application/xml", "application/json"})
+	public void CreateUpdateOrDeleteBid(
+			@PathParam("identity") long identity,
+			@HeaderParam("Authorization") final String authentication,
+			@Valid @NotNull Bid template
+			){
+		
+		final EntityManager brokerManager = LifeCycleProvider.brokerManager();
+		final Person requester = LifeCycleProvider.authenticate(authentication);
+		
+		final boolean persist = template.getIdentity() == 0;
+		
+		Auction auction = brokerManager.find(Auction.class, identity);
+		if (auction == null) throw new NotFoundException();
+				
+		final Bid bid;
+		if(persist){
+			bid = new Bid(auction, requester);
+		} else  {
+			bid = brokerManager.find(Bid.class, template.getIdentity());
+			if (bid == null) throw new NotFoundException();
+			if (bid.getBidderReference() != requester.getIdentity()) throw new ForbiddenException();
+		}
+			
+		if (!persist && template.getPrice() == 0){
+			brokerManager.remove(bid);
+		} else{
+			bid.setPrice(template.getPrice());
+			bid.setVersion(template.getVersion());
+			
+		}
+
+		try {
+			if (persist) brokerManager.persist(bid);	
+			else brokerManager.flush();
+		} catch (ConstraintViolationException e) {
+			throw new ClientErrorException(Status.BAD_REQUEST);
+		}
+
+		try {
+			brokerManager.getTransaction().commit();
+		} catch (RollbackException e) {
+			throw new ClientErrorException(Status.CONFLICT);
+		}
+		finally {
+			brokerManager.getTransaction().begin();
+		}
+	}
 }
 
 
--- broker-model/src/de/sb/broker/rest/PersonService.java
+++ broker-model/src/de/sb/broker/rest/PersonService.java
@@ -12,13 +12,24 @@ import java.util.stream.Stream;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.RollbackException;
 import javax.persistence.TypedQuery;
+import javax.validation.ConstraintViolationException;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.ClientErrorException;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.ForbiddenException;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
 //import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response.Status;
 
 import de.sb.broker.model.Address;
 import de.sb.broker.model.Auction;
@@ -148,8 +159,7 @@ public class PersonService {
 			 * 
 			 * else: create new person
 			 * 
-			 */
-			
+			 */			
 			/*
 			cache = em.getEntityManagerFactory().getCache();
 			cache.evict(entity.getClass(), entity.getIdentity());
@@ -158,7 +168,76 @@ public class PersonService {
 		} finally {
 			em.getTransaction().begin();
 		}
+	}
+	
+	/*
+	 * GET /people/requester (new): 
+	 * Returns the authenticated requester, which is useful for login operations.
+	 */
+	@GET
+	@Path("/requester")
+	@Produces({"application/xml", "application/json"})
+	public Person getRequester(
+			@HeaderParam("Authorization") final String authentication){
+		return LifeCycleProvider.authenticate(authentication);
+	}
 		
+	/*
+	 * PUT /people: 
+	 * Requesters that are not part of group ADMIN are both forbidden 
+	 * to alter other people, and to set their own group to ADMIN.
+	 */
+	@PUT
+	@Consumes({"application/xml", "application/json"})
+	public Long createOrUpdatePerson(
+			@Valid @NotNull Person template,
+			@HeaderParam("Authorization") final String authentication,
+			@HeaderParam("Password") final String password) {
+		
+		final EntityManager brokerManager = LifeCycleProvider.brokerManager();
+		final Person requester = LifeCycleProvider.authenticate(authentication);
+		
+		final boolean persist = template.getIdentity() == 0;
+		final Person person;
+		if(persist){
+			person = new Person();
+		} else if (requester.getGroup() == Group.ADMIN || requester.getIdentity() == template.getIdentity()){
+			person = brokerManager.find(Person.class, template.getIdentity());
+			if (person == null) throw new NotFoundException();
+		} else {
+			throw new ForbiddenException();
+		}
+		
+		
+		person.setAlias(template.getAlias());
+		person.setGroup(template.getGroup());
+		person.getName().setFamily(template.getName().getFamily());
+		person.getName().setGiven(template.getName().getGiven());
+		person.getAddress().setCity(template.getAddress().getCity());
+		person.getAddress().setPostCode(template.getAddress().getPostCode());
+		person.getAddress().setStreet(template.getAddress().getStreet());
+		person.getContact().setEmail(template.getContact().getEmail());
+		person.getContact().setPhone(template.getContact().getPhone());
+		person.setPasswordHash(password.getBytes());
+		person.setVersion(template.getVersion());
+		
+		try {
+			if (persist) brokerManager.persist(person);	
+			else {
+				brokerManager.flush();
+			}
+		} catch (ConstraintViolationException e) {
+			throw new ClientErrorException(Status.BAD_REQUEST);
+		}
 
+		try {
+			brokerManager.getTransaction().commit();
+		} catch (RollbackException e) {
+			throw new ClientErrorException(Status.CONFLICT);
+		}
+		finally {
+			brokerManager.getTransaction().begin();
+		}
+		return person.getIdentity();
 	}
 }
