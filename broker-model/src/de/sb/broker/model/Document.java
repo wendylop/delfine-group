ec01ebe2d76ee5fa3199c5914ba4010d2648d373
Lilian Hung <l.hung.m@gmail.com>
2016-12-02 14:03:54 +0100

    added Document class, needs implementation

---
+++ broker-model/src/de/sb/broker/model/Document.java
@@ -0,0 +1,13 @@
+package de.sb.broker.model;
+
+public class Document {
+	/* TODO implement class
+	 * documentIdentity BIGINT NOT NULL,
+	 * content MEDIUMBLOB NOT NULL,
+	 * contentHash BINARY(32) NOT NULL,
+	 * contentType CHAR(64) NOT NULL,
+	 */
+	
+	
+
+}
